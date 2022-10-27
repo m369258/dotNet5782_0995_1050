7ffe1b0df1efb0f34abb1099f9f1b58305b57322
michal <michal.grinboim@gmail.com>
2022-10-27 21:43:38 +0300

    inital code +  welcome functions

---
+++ Stage0/Program1826.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace Targil0
+{
+  partial class Program
+    {
+        static void Main()
+        {
+            Welcome1826();
+            Welcome3114();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome3114();
+        private static void Welcome1826()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+
+    }
+}
