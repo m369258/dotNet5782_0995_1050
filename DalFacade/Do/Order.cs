fedbc820e5e98f02664ab8b74d3b1fabdf735df1
michal <michal.grinboim@gmail.com>
2022-10-31 20:00:10 +0200

    stage1 + page 6

--- DalFacade/Do/Order.cs
+++ DalFacade/Do/Order.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Do;
 
-namespace Do
+/// <summary>
+/// 
+/// </summary>
+public struct Order
 {
-    internal class Order
-    {
-    }
 }
