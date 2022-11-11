f915a7eccebf7afb5889aa1f8660bb29555e376b
michal <michal.grinboim@gmail.com>
2022-11-11 10:33:40 +0200

    stage1 till page 9

--- DalList/DalOrder.cs
+++ DalList/DalOrder.cs
@@ -9,38 +9,67 @@ namespace Dal
 {
     public class DalOrder
     {
-
+        /// <summary>
+        /// This action adds an order to the system if there is an available space
+        /// </summary>
+        /// <param name="order">Order to add</param>
+        /// <returns>Return the ID number of the added object</returns>
+        /// <exception cref="Exception">If there is no space available for a new order, an error will be thrown</exception>
         public int Add(Order order)
         {
             order.ID = DataSource.Config.AutomaticOrder;
+            //Checking whether there is room to add an order otherwise an error will be thrown
             if (DataSource.orders.Length - 1 != DataSource.Config.indexOrder)
             {
                 DataSource.Add(order);
             }
             else
             {
-                throw new Exception("there are no place");
+                throw new Exception("there is no place");
             }
             return order.ID;
         }
 
+
+        /// <summary>
+        /// This returns the correct order by some ID number
+        /// </summary>
+        /// <param name="idOrder">ID number of desired order</param>
+        /// <returns>Returns the desired order</returns>
+        /// <exception cref="Exception">If the required order does not exist, an error will be thrown</exception>
         public Order Get(int idOrder)
         {
             int i = 0;
+            //The loop searches for the location of the order to be deleted
             while (i < DataSource.Config.indexOrder && DataSource.orders[i].ID != idOrder)
             {
                 i++;
             }
+            //Checking whether the requested order is found and returning it otherwise throws an error
             if (DataSource.orders[i].ID == idOrder)
                 return DataSource.orders[i];
             throw new Exception("there are no order with this id");
         }
 
+        /// <summary>
+        /// This returns all orders
+        /// </summary>
+        /// <returns>All orders</returns>
         public Order[] GetAllOrders()
         {
+            Order[] newOrders = new Order[DataSource.Config.indexOrder];
+            for(int i=0;i<DataSource.Config.indexOrder;i++)
+            {
+
+            }
             return DataSource.orders;
         }
 
+        /// <summary>
+        /// This operation gets an order ID number and deletes it if it exists, otherwise an error will be thrown
+        /// </summary>
+        /// <param name="idOrder">Order ID number</param>
+        /// <exception cref="Exception">In case the order does not exist in the database, an error will be thrown</exception>
         public void Delete(int idOrder)
         {
             int ind = GetIndex(idOrder);
@@ -61,39 +90,43 @@ namespace Dal
             }
             else
                 throw new Exception("there is no this id order");
-            }
-
-            private int GetIndex(int idOrder)
-            {
-                int i = 0;
-                while (i < DataSource.Config.indexOrder && DataSource.orders[i].ID != idOrder)
-                {
-                    i++;
-                }
-                if (DataSource.orders[i].ID == idOrder)
-                    return i;
-                return -1;
-            }
         }
 
-        public void Update()
+
+        private int GetIndex(int idOrder)
         {
-
+            int i = 0;
+            while (i < DataSource.Config.indexOrder && DataSource.orders[i].ID != idOrder)
+            {
+                i++;
+            }
+            if (DataSource.orders[i].ID == idOrder)
+                return i;
+            return -1;
+        }
+        public void Update(Order updateOrder)
+        {
+            int ind = GetIndex(updateOrder.ID);
+            if (ind != -1)
+            {
+                DataSource.orders[ind] = updateOrder;//??האם זה נחשב להעתקה עמוקה
+            }
+            else { throw new Exception("there is no order like this"); }
         }
 
 
 
-
-        //public int Add(Order order)
-        //{
-        //    order.ID = DataSource.Config.NextOrderNumber;//שמירה מה ה ID של האורדר וגם ;
-        //    DataSource.OrdersList.Add(order);//להחליט אם לעשות מערך או רשימה
-        //    if(DataSource.config.indexinthearray>=גודל מערך)//אם זה מערך
-        //            throw "אין מקום";
-        //    //ואם יש מקום במערך אז צריך ללכת ולהוסיף
-        //    return order.ID;
-        //}
-
-        //pubic order GetById(int id)
     }
+    //public int Add(Order order)
+    //{
+    //    order.ID = DataSource.Config.NextOrderNumber;//שמירה מה ה ID של האורדר וגם ;
+    //    DataSource.OrdersList.Add(order);//להחליט אם לעשות מערך או רשימה
+    //    if(DataSource.config.indexinthearray>=גודל מערך)//אם זה מערך
+    //            throw "אין מקום";
+    //    //ואם יש מקום במערך אז צריך ללכת ולהוסיף
+    //    return order.ID;
+    //}
+
+    //pubic order GetById(int id)
 }
+
