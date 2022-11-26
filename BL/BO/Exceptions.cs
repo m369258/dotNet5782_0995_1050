c28393649f7c20a4f0211ce13b13478e761510cd
nusbis <shira6557@gmail.com>
2022-11-26 20:20:04 +0200

    stage2+ 26/11

--- BL/BO/Exceptions.cs
+++ BL/BO/Exceptions.cs
@@ -1,6 +1,22 @@
-﻿
-namespace BO;
+﻿namespace BO;
 
-internal class Exceptions
+/// <summary>
+/// Error if the index is not correct
+/// </summary>
+public class IncorrectIndex : Exception
 {
+    public IncorrectIndex(string? message) : base(message) { }
+}
+
+/// <summary>
+/// Throw an error in case the field is invalid
+/// </summary>
+public class InvalidField: Exception
+{
+    public InvalidField(string? message) : base(message) { }
+}
+
+public class CannotPerformThisOperation : Exception
+{
+    public CannotPerformThisOperation(string? message) : base(message) { }
 }
