e8bed3567018ff4f2510bec0789bc91425030a78
michal <michal.grinboim@gmail.com>
2022-12-02 15:50:10 +0200

    stage2 - 01/12

--- BL/BlApi/IOrder.cs
+++ BL/BlApi/IOrder.cs
@@ -2,7 +2,17 @@
 
 public interface IOrder
 {
+    /// <summary>
+    /// Order list request
+    /// </summary>
+    /// <returns>Order list</returns>
     public IEnumerable<BO.OrderForList> GetListOfOrders();
+
+    /// <summary>
+    /// Order shipping update
+    /// </summary>
+    /// <param name="orderId">the order id to update</param>
+    /// <returns>the update order</returns>
     public BO.Order OrderShippingUpdate(int orderId);
 
     public BO.Order GetOrderDetails(int idOrder);
