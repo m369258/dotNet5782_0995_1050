c4927e4bdbec3529cfdcae25d61f4a0fc262af08
nusbis <shira6557@gmail.com>
2022-12-14 21:14:32 +0200

    stage3+PL

--- PL/Product/ProductForListWindow.xaml.cs
+++ PL/Product/ProductForListWindow.xaml.cs
@@ -24,17 +24,43 @@ namespace PL.Product
         {
             InitializeComponent();
             ProductListview.ItemsSource = bl.product.GetListOfProducts();
-            CategorySelector.ItemsSource = Enum.GetValues(typeof(BO.Category));
-            //btnClear.IsEnabled = false;
-
+            cmxCategorySelector.ItemsSource = Enum.GetValues(typeof(BO.Category));
+            btnClear.Visibility = Visibility.Hidden;
         }
+
 
         private void CategorySelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-           BO.Category  currCategorySelect = (BO.Category)CategorySelector.SelectedItem;
-            
-            ProductListview.ItemsSource=bl.product.GetListOfProducts((int)currCategorySelect);
-           // btnClear.IsEnabled=true;
+            if(cmxCategorySelector.SelectedIndex!=-1)
+            {
+                BO.Category currCategorySelect = (BO.Category)cmxCategorySelector.SelectedItem;
+                ProductListview.ItemsSource = bl.product.GetListOfProducts((int)currCategorySelect);
+                btnClear.Visibility = Visibility.Visible;
+            }  
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            ProductListview.ItemsSource = bl.product.GetListOfProducts();
+            cmxCategorySelector.SelectedIndex = -1;
+            btnClear.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new ProductWindow().ShowDialog();
+            ProductListview.ItemsSource = bl.product.GetListOfProducts();
+        }
+
+        private void ProductListview_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ProductListview.SelectedIndex != -1)
+            {
+                new ProductWindow(((BO.ProductForList)ProductListview.SelectedItem).ID).ShowDialog();
+                ProductListview.SelectedIndex = -1;
+                ProductListview.ItemsSource = bl.product.GetListOfProducts();
+            }
+
         }
     }
 }
