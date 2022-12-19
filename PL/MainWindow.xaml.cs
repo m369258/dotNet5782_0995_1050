f55d0050df4cc2e012463f5b2c208d9bd3b7b1f2
michal <michal.grinboim@gmail.com>
2022-12-19 16:18:53 +0200

    all stage3: An experiment of adding an icon to the code, in the previous push the git was stuck with it and it was wrong, so we took it down.

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -10,15 +10,9 @@ namespace PL;
 /// </summary>
 public partial class MainWindow : Window
 {
-    private BlApi.IBl bl = new BlApi.Bl();
-    public System.Windows.Media.ImageSource Icon { get; set; }
-
     public MainWindow()
     {
         InitializeComponent();
-        Uri iconUri = new Uri("pack://application:,,,/WPFIcon2.ico", UriKind.RelativeOrAbsolute);
-        this.Icon = BitmapFrame.Create(iconUri);
-
     }
 
     private void ShowProductsButton_Click(object sender, RoutedEventArgs e) => new ProductForListWindow().Show();
