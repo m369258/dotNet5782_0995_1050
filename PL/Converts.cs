df555cb28684c9b5003736e637f5cbda0cadaf84
nusbis <shira6557@gmail.com>
2023-02-17 12:23:55 +0200

    hhh

--- PL/Converts.cs
+++ PL/Converts.cs
@@ -26,107 +26,24 @@ public class ConverIntToString : IValueConverter
     }
 }
 
-
-public class NotBooleanToVisibilityConverter : IValueConverter
-{
-    //convert from source property type to target property type
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        BO.Cart cart = (BO.Cart)value;
-        if (cart != null && cart.items != null && cart.items.Count != 0)
-        {
-            return Visibility.Hidden; //Visibility.Collapsed;
-        }
-        else
-        {
-            return Visibility.Visible;
-        }
-    }
-
-    //convert from target property type to source property type
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        throw new NotImplementedException();
-    }
-
-}
-
-public class NotBooleanToVisibilityConverter2 : IValueConverter
-{
-    //convert from source property type to target property type
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        BO.Cart cart = (BO.Cart)value;
-        if (cart != null && cart.items != null && cart.items.Count != 0)
-        {
-            return Visibility.Visible; //Visibility.Collapsed;
-        }
-        else
-        {
-            return Visibility.Hidden;
-        }
-    }
-
-    //convert from target property type to source property type
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        throw new NotImplementedException();
-    }
-}
-
-
-
-
-/// <summary>
-/// convert image path to picture
-/// </summary>
-class ConvertPathToBitmapImage : IValueConverter
-{
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        try
-        {
-            string imageRelativeName = (string)value;
-            string currentDir = Environment.CurrentDirectory[..^4];
-            string imageFullName = currentDir + imageRelativeName;//direction of the picture
-            BitmapImage bitmapImage = new BitmapImage(new Uri(imageFullName));//makes the picture
-            return bitmapImage;
-        }
-        catch
-        {
-            string imageRelativeName = @"\pics\default.jpg";//default picture
-            string currentDir = Environment.CurrentDirectory[..^4];
-            string imageFullName = currentDir + imageRelativeName;//direction of the picture
-            BitmapImage bitmapImage = new BitmapImage(new Uri(imageFullName));//makes the picture
-            return bitmapImage;
-        }
-    }
-
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        throw new NotImplementedException();
-    }
-}
-}
-
 public class NotBooleanToVisibileConverter : IValueConverter
 {
-    //convert from source property type to target property type
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    //convert from source property type to target property type
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         bool isVisible = (bool)value;
         if (isVisible)
         {
             return Visibility.Visible; //Visibility.Collapsed;
-        }
+        }
         else
         {
             return Visibility.Hidden;
         }
     }
 
-    //convert from target property type to source property type
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    //convert from target property type to source property type
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
     }
@@ -146,3 +63,4 @@ public class NotBooleanToVisibilityConverter : IValueConverter
         throw new NotImplementedException();
     }
 }
+
