b66dffb63ea2e56d62a91172b9d751d294190502
nusbis <shira6557@gmail.com>
2023-02-19 21:02:36 +0200

    stage5===

--- PL/Converts.cs
+++ PL/Converts.cs
@@ -1,15 +1,9 @@
-﻿using BO;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Collections.ObjectModel;
 using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Security.Permissions;
-using System.Threading;
 using System.Windows;
 using System.Windows.Data;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 namespace PL;
 
